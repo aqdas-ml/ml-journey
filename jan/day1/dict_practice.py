856a5bf257e6655135240a4d40bcc45d34eadb63
aqdas-ml <ansariaqdas021@gmail.com>
2026-01-01 11:53:40 +0530

    Add day1 dictionary practice script

---
+++ jan/day1/dict_practice.py
@@ -0,0 +1,13 @@
+items = ["Apple", "Banana", "Apple", "Orange", "Banana", "Apple"]
+
+frequency = {}
+
+for i in items:
+    if i in frequency:
+        frequency[i] += 1
+      
+    else:
+        frequency[i] = 1
+
+print("Frequency count:")
+print(frequency)
