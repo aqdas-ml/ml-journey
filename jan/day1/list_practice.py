ef0587b72f8fc6a94b5e6ad386dc0ad12e0dc0c5
aqdas-ml <ansariaqdas021@gmail.com>
2026-01-01 11:51:20 +0530

    Add day1 list practice script

---
+++ jan/day1/list_practice.py
@@ -0,0 +1,8 @@
+Numbers = [5, 76, 87, 100, 54, 202, 1]
+
+print(f"Original List :- {Numbers}")
+print(f"Maximum Number :- {max(Numbers)}")
+print(f"Minimum Number :- {min(Numbers)}")
+
+Reversed_list = Numbers[::-1]
+print(f"Reversed List :- {Reversed_list}")
