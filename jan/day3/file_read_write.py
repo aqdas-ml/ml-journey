7719f8b511e66be1419f983178a0860787f34c84
Mohammad Aqdas Ansari <ansariaqdas021@gmail.com>
2026-01-03 12:47:57 +0530

    Add day 3 file handling practice scripts

---
+++ jan/day3/file_read_write.py
@@ -0,0 +1,8 @@
+with open("sample.txt","w") as file:
+    file.write("Machine Learning starts with strong Python basics.\n")
+    file.write("This is Day 3 practice.\n")
+
+with open("sample.txt","r") as file:
+    Content = file.read()
+print("File Content:")
+print(Content)
