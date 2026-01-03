1185e08d5e4be4403606a001ec25ed86e1107435
Mohammad Aqdas Ansari <ansariaqdas021@gmail.com>
2026-01-03 12:50:57 +0530

    Add day 3 file handling (words count and lines count) practice scripts

---
+++ jan/day3/word_count.py
@@ -0,0 +1,11 @@
+with open("sample.txt", "r") as file:
+    lines = file.readlines()
+
+line_count = len(lines)
+word_count = 0
+
+for line in lines:
+    words = line.split()
+    word_count += len(words)
+print("Number of lines",line_count)
+print("Number of words",word_count)
