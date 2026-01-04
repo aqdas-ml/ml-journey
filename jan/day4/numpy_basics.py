43f8f0f79f8fec1fb449a789ccbd35810581b8c9
Mohammad Aqdas Ansari <ansariaqdas021@gmail.com>
2026-01-04 14:57:42 +0530

    Add day 4 NumPy basics and array operations

---
+++ jan/day4/numpy_basics.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr = np.array([10,20,30,40,50])
+
+print("Array",arr)
+print("Shape:", arr.shape)
+print("Data type:", arr.dtype)
+
+print("First element:", arr[0])
+print("Slice:", arr[1:4])
