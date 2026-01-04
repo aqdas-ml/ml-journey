c18f5473952f5fa290639c7d9869e736d0242b04
Mohammad Aqdas Ansari <ansariaqdas021@gmail.com>
2026-01-04 14:58:47 +0530

    Add day 4 NumPy basics and array operations

---
+++ jan/day4/numpy_math.py
@@ -0,0 +1,10 @@
+import numpy as np
+a = np.array([1, 2, 3, 4])
+b = np.array([5, 6, 7, 8])
+
+print("Addition:", a + b)
+print("Multiplication:", a * b)
+print("Sum:", a.sum())
+print("Mean:", a.mean())
+print("MIn",a.min())
+print("Max:", a.max())
